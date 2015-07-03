cf7f921600e9149fab2d9b69955712dbea064af5
Marco Pedicini <marco@iac.cnr.it>
2015-07-04 00:11:57 +0200

    This is the first public release of PELCR

--- h/buildgraph.h
+++ h/buildgraph.h
@@ -1,3 +1,22 @@
+/* Syntax interpretation function used in the parser
+ 
+ Copyright (C) 1997-2015 Marco Pedicini
+ 
+ This file is part of PELCR.
+ 
+ PELCR is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ PELCR is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with PELCR.  If not, see <http://www.gnu.org/licenses/>.  */
+
 #ifndef bgraph_h
 #define bgraph_h
 
--- main.c
+++ main.c
@@ -1,3 +1,24 @@
+/* Main part of the source code: interpretation of commandline options,
+ starting of execution, printing of messages.
+ 
+ Copyright (C) 1997-2015 Marco Pedicini
+ 
+ This file is part of PELCR.
+ 
+ PELCR is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ PELCR is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with PELCR.  If not, see <http://www.gnu.org/licenses/>.  */
+
+
 #include <stddef.h>
 #include <unistd.h>
 #include <math.h>
